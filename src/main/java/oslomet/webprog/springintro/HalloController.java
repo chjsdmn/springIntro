ddd234b04534b5c486cedea6bb2d7b311d02776c
chjsdmn <chjsdmn@hotmail.com>
2022-02-15 13:11:41 +0100

    la til Kunde registrer

--- src/main/java/oslomet/webprog/springintro/HalloController.java
+++ src/main/java/oslomet/webprog/springintro/HalloController.java
@@ -2,7 +2,11 @@ package oslomet.webprog.springintro;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 如果有改动，或者其他static里的文件有改动，都要按绿色的小锤子重新编译，或快捷键ctrl+F9
@@ -14,5 +18,19 @@ public class HalloController {
     String hallo(String navn){
         int length = navn.length();
         return "Hallo " + navn + ", navnet ditt er " + length + " ord langt.";
+    }
+
+    public final List<Kunde> kunder = new ArrayList<>();  //常用写法final， 用List代替ArrayList也算是一个惯用方法
+    @PostMapping("/lagrekunde")
+    void regisKunde (Kunde innKunde){
+        kunder.add(innKunde);
+    }
+    @GetMapping("/henteAlleKunder")
+    List henteAlleKunder(){
+        return kunder;
+    }
+    @GetMapping("/slettKunder")
+    void slettAlleKunder(){
+        kunder.clear();
     }
 }
